fbd5d7a8507c400d24b723332448fe5b640635b5
NAKNAO-nnct <nakanoyuki@team-lab.com>
2025-06-03 19:01:23 +0900

    fix: entorypoint sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
-nohup /usr/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 --storage-driver=fuse-overlayfs &
+#!/bin/bash
+
+set -e
+
+# 127.0.0.1:2375 が起動しているか確認
+until nc -z -v -w30 127.0.0.1 2375
+do
+    echo "waiting for docker host to start on"
+    sleep 5
+done
+
+/usr/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 --storage-driver=fuse-overlayfs &
 
 # docker info が表示されるまで待つ
 while ! docker info > /dev/null 2>&1; do
@@ -6,4 +17,3 @@ while ! docker info > /dev/null 2>&1; do
     sleep 1
 done
 echo "Docker is running."
-
